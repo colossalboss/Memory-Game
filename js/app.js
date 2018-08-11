dae93395d163ce8c8c3c40d03833b61be88ce217
Ogbara G. Godstar <colossalboss@users.noreply.github.com>
2018-08-11 18:18:01 +0100

    Update restart functionality and add comments

--- js/app.js
+++ js/app.js
@@ -1,165 +1,227 @@
 /*
- * Create a list that holds all of your cards
- */
+* Icons
+*/
 const icons = [
-"fa fa-diamond",
-"fa fa-diamond",
-"fa fa-paper-plane-o",
-"fa fa-paper-plane-o",
-"fa fa-anchor",
-"fa fa-anchor",
-"fa fa-bolt",
-"fa fa-bolt",
-"fa fa-cube",
-"fa fa-cube",
-"fa fa-leaf",
-"fa fa-leaf",
-"fa fa-bicycle",
-"fa fa-bicycle",
-"fa fa-bomb",
-"fa fa-bomb",
+  "fa fa-cube",
+  "fa fa-leaf",
+  "fa fa-leaf",
+  "fa fa-bicycle",
+  "fa fa-bicycle",
+  "fa fa-bomb",
+  "fa fa-diamond",
+  "fa fa-diamond",
+  "fa fa-paper-plane-o",
+  "fa fa-paper-plane-o",
+  "fa fa-anchor",
+  "fa fa-anchor",
+  "fa fa-bolt",
+  "fa fa-bolt",
+  "fa fa-cube",
+  "fa fa-bomb",
 ];
 
+// container for list items
+let container = document.querySelector('.deck');
+//Create an array to hold selected cards
+let openCards = [];
+//Array to hold matched cards
+let matchedCards = [];
+
 /*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+*Create list of cards
+*/
+function init() {
+  container.innerHTML = '';
+  let collection = shuffle(icons);
+  for (let i = 0; i < collection.length; i++) {
+    const card = document.createElement('li');
+    card.classList.add('card');
+    card.innerHTML = `<i class="${collection[i]}"></i>`;
+    container.appendChild(card);
 
- let container = document.querySelector('.deck');
+  click(card);
+  }
 
- let openCards = [];
- let matchedCards = [];
+  timer();
 
+  stars.innerHTML = `	<li><i class="fa fa-star"></i></li>
+    <li><i class="fa fa-star"></i></li>
+    <li><i class="fa fa-star"></i></li>`;
+    moves = 0;
+    movesCon.innerHTML = moves;
+}
 
- function init() {
-   let shuffledIcons = shuffle(icons);
-   for (let i = 0; i < shuffledIcons.length; i++) {
-     const card = document.createElement('li');
-     card.classList.add('card');
-     card.innerHTML = `<i class="${shuffledIcons[i]}"></i>`;
-     container.appendChild(card);
+/*
+*Handle Clicks
+*/
+function click(card) {
+  card.addEventListener('click', function() {
+    let currentCard = this;
+    let previousCard = openCards[0];
 
-    click(card);
-   }
- }
+    if (openCards.length === 1) {
+      card.classList.add('open', 'show', 'disable');
+      openCards.push(this);
 
- function click(card) {
-   // click event
-   card.addEventListener('click', function() {
-     let currentCard = this;
-     let previousCard = openCards[0];
+      compare(currentCard, previousCard);
 
-     if (openCards.length === 1) {
-       card.classList.add('open', 'show', 'disable');
-       openCards.push(this);
+    } else {
+      card.classList.add('open', 'show', 'disable');
+      openCards.push(this);
+	}
 
-       compare(currentCard, previousCard);
-
-     } else {
-       card.classList.add('open', 'show', 'disable');
-       openCards.push(this);
-     }
-
-   });
- }
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
+  });
 }
 
 
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (currentIndex !== 0) {
+	randomIndex = Math.floor(Math.random() * currentIndex);
+	currentIndex -= 1;
+	temporaryValue = array[currentIndex];
+	array[currentIndex] = array[randomIndex];
+	array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+*Compare selected cards 
+*/
+function compare(currentCard, previousCard) {
+if (currentCard.innerHTML === previousCard .innerHTML) {
+	currentCard.classList.add('match');
+	previousCard.classList.add('match');
+	matchedCards.push(currentCard, previousCard);
+	openCards = [];
+	gameOver();
+} else {
+currentCard.classList.add('red');
+previousCard.classList.add('red');
+	setTimeout(function() {
+	
+	currentCard.classList.remove('open', 'show', 'disable');
+	previousCard.classList.remove('open', 'show', 'disable');
+
+	currentCard.classList.remove('red');
+	previousCard.classList.remove('red');
+
+	openCards = [];
+	}, 500);
+}
+
+addMove();
+}
+
+//Rating stars
+const stars = document.querySelector('.stars');
+
+/*
+*Rate according to number of moves
+*/
+function rate() {
+if (moves > 10 && moves <= 20) {
+	stars.innerHTML = `<li><i class="fa fa-star"></i></li>
+	<li><i class="fa fa-star"></i></li>`;
+} else if (moves > 20) {
+	stars.innerHTML = `<li><i class="fa fa-star"></i></li>`;
+} else {
+	stars.innerHTML = `	<li><i class="fa fa-star"></i></li>
+	<li><i class="fa fa-star"></i></li>
+	<li><i class="fa fa-star"></i></li>`;
+	}
+}
+
+ /*
+ *When all cards are matched
  */
-
- function compare(currentCard, previousCard) {
-   if (currentCard.innerHTML === previousCard .innerHTML) {
-     currentCard.classList.add('match');
-     previousCard.classList.add('match');
-     matchedCards.push(currentCard, previousCard);
-     openCards = [];
-     gameOver();
-   } else {
-     setTimeout(function() {
-       currentCard.classList.remove('open', 'show', 'disable');
-       previousCard.classList.remove('open', 'show', 'disable');
-       openCards = [];
-     }, 500);
-   }
-
-   addMove();
- }
-
- const stars = document.querySelector('.stars');
-
- function rate() {
-   if (moves > 10 && moves < 16) {
-     stars.innerHTML = `<li><i class="fa fa-star"></i></li>
-     <li><i class="fa fa-star"></i></li>`;
-   } else if (moves >= 16) {
-     stars.innerHTML = `<li><i class="fa fa-star"></i></li>`;
-   } else {
-     stars.innerHTML = `	<li><i class="fa fa-star"></i></li>
-       <li><i class="fa fa-star"></i></li>
-       <li><i class="fa fa-star"></i></li>`;
-   }
- }
-
  function gameOver() {
    if (matchedCards.length === icons.length) {
+     //After 500ms
      setTimeout(function () {
-       alert('Game Over');
-       container.innerHTML = '';
-       init();
+      container.innerHTML = '';
+      
+      container.innerHTML = `
+        <div id="over">
+          <p>Congrats! You won</p>
+          <p>
+            Moves ${moves} <br>
+            Time ${duration} seconds <br>
+            <ul class="rate">${stars.innerHTML}</ul>
+          </p>
+          <button id="again" onclick="playAgain();">Restart</button>
+        </div>
+      `;
      }, 500);
+     //stop timer
+     stop();
    }
  }
 
+//moves container
 const movesCon = document.querySelector('.moves');
+//time container
+const timeCon = document.querySelector('.time');
+//moves
 let moves = 0;
 movesCon.innerHTML = 0;
+
+//Handle moves
  function addMove() {
    moves++;
    movesCon.innerHTML = moves;
-
+  //Calculate stars
    rate();
  }
+// Time
+ function changeValue() {
+  timeCon.innerHTML = ++duration;
+}
 
- const restartBtn = document.querySelector('.restart');
- restartBtn.addEventListener('click', function() {
-   // clear all cards
-   container.innerHTML = '';
+let timerInterval = null;
+function start() {
+  // stop();
+  duration = 0;
+  timerInterval = setInterval(changeValue, 1000);
+  container.removeEventListener('click', start);
+}
+let stop = function() {
+  clearInterval(timerInterval);
+}
 
-   // create new cards
-   init();
+//Starts timer on the first click
+function timer() {
+  container.addEventListener('click', start);
+}
 
-   // reset other variables
-   matchedCards = [];
-   moves = 0;
-   movesCon.innerHTML = moves;
-   stars.innerHTML = 	`<li><i class="fa fa-star"></i></li>
-     <li><i class="fa fa-star"></i></li>
-     <li><i class="fa fa-star"></i></li>`;
- })
+//Select the restart icon
+const restartBtn = document.querySelector('.restart');
+//Restart Game on clicking the restart icon
+restartBtn.addEventListener('click', playAgain);
 
+/*
+*Handle Restart
+*/
+ function playAgain() {
+  // clear all cards
+  container.innerHTML = '';
+  // create new cards
+  init();
+
+  // reset other variables
+  timeCon.innerHTML = 0;
+  matchedCards = [];
+  moves = 0;
+  movesCon.innerHTML = moves;
+  stars.innerHTML = 	`<li><i class="fa fa-star"></i></li>
+    <li><i class="fa fa-star"></i></li>
+    <li><i class="fa fa-star"></i></li>`;
+}
+
+//Starting the game for the first time
 init();
